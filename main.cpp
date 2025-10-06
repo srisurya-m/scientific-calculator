7b04a28bf04665db4f2b13e7b3bb15c396ef0e6e
srisurya-m <srisurya.makkapati@gmail.com>
2025-10-06 08:13:51 +0530

    test project -- 6

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 int main() {
     CalculatorFacade calc;
 
-    std::cout << "Sqrt(16) = " << calc.perform("sqrt", 16) << std::endl;
+    std::cout << "Sqrt(16) = " << calc.perform("sqrt", 256) << std::endl;
     std::cout << "Factorial(5) = " << calc.perform("fact", 5) << std::endl;
     std::cout << "Ln(10) = " << calc.perform("ln", 10) << std::endl;
-    std::cout << "Power(2, 8) = " << calc.perform("pow", 2, 8) << std::endl;
+    std::cout << "Power(2, 8) = " << calc.perform("pow", 2, 4) << std::endl;
 
     return 0;
 }
